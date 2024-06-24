2888f4418ce0c72781c3e5bf58c7bf5b968cdd05
unknown <mialimo19@gmail.com>
2024-06-24 12:44:52 +0700

    first

---
+++ script.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.hero-text', { delay: 200, origin: 'top' });
+sr.reveal('.img-hero', { delay: 450, origin: 'top' });
+sr.reveal('.icons', { delay: 500, origin: 'top' });
+sr.reveal('.chat', { delay: 500, origin: 'right' });
